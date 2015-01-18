47eaae49b238e2c96af5690b7e5d7ae6f4da38db
Jonathan Narzo <jonathan.narzo@gmail.com>
2015-01-18 15:59:04 +0800

    database migration for products table

---
+++ app/database/migrations/2015_01_18_075744_products.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Products extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('products', function($table){
+			$table->increments('id');
+			$table->integer('category_id');
+			$table->integer('brand_id');
+			$table->string('name');
+			$table->text('description');
+			$table->string('image');
+			$table->integer('quantity');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('products');
+	}
+
+}
