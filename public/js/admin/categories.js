2eef92b04f3ac62a8db54a6231726e18beb21526
Jonathan Narzo <jonathan.narzo@gmail.com>
2015-02-08 18:06:56 +0800

    moved some scripts to admin.js

--- public/js/admin/categories.js
+++ public/js/admin/categories.js
@@ -19,18 +19,6 @@ $('.edit-view').click(function() {
 	});
 });
 
-/* Delete Confirmation */
-$('.confirm-delete').click(function(){
-	var con = confirm('Are you sure to delete record?');
-	return con;
-});
-
-/* Form Delete Confirmation */
-$('.form-delete-confirm').submit(function(){
-	var con = confirm('You are about to delete record/s?');
-	return con;
-});
-
 /* Multiple Input forms */ 
 $(function() {
     var appendTbl = $('#input-tbl');
